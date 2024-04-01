98ba2557106a77320e6e377ce51d1f3c449e9208
Serkan <serkanbay02@hotmail.com>
2024-04-01 12:44:38 +0300

    initial setup with nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F6FFF8",
+      },
+    },
+  },
+  plugins: [],
+};
