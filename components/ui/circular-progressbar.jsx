1a75ae45db11a0e4008a1d821ae03806663c8e7d
Serkan <serkanbay02@hotmail.com>
2024-04-03 12:53:33 +0300

    get started with animated circular bar

--- components/ui/circular-progressbar.jsx
+++ components/ui/circular-progressbar.jsx
@@ -1,20 +1,61 @@
-import { Dimensions, StyleSheet, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import Svg, { Circle } from "react-native-svg";
 
 const BACKGROUND_COLOR = "#444B6F";
 const BACKGROUND_STROKE_COLOR = "#303858";
 const STROKE_COLOR = "#A6E1FA";
 
-const { width, height } = Dimensions.get("window");
-
 const CIRCLE_LENGTH = 400;
 const R = CIRCLE_LENGTH / (2 * Math.PI);
 
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+
 export function ProgressBar() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withTiming(1, { duration: 2000 });
+  }, []);
+
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value),
+  }));
+
   return (
-    <View className="flex-1 items-center justify-center">
+    <View
+      onLayout={(event) => {
+        const { width, height } = event.nativeEvent.layout;
+
+        setWidth(width);
+        setHeight(height);
+      }}
+      className="flex-1 items-center justify-center border"
+    >
       <Svg>
-        <Circle cx="50%" cy="50%" r={R} stroke={"red"} strokeWidth={10} />
+        {width > 0 && (
+          <>
+            <AnimatedCircle
+              fillOpacity={0}
+              strokeDasharray={CIRCLE_LENGTH}
+              animatedProps={animatedProps}
+              cx={width / 2}
+              cy={height / 2}
+              r={R}
+              stroke={"#C4E4FF"}
+              strokeWidth={15}
+              strokeLinecap={"round"}
+            />
+          </>
+        )}
       </Svg>
     </View>
   );
@@ -36,7 +77,6 @@ const styles = StyleSheet.create({
   button: {
     position: "absolute",
     bottom: 80,
-    width: width * 0.7,
     height: 60,
     backgroundColor: BACKGROUND_STROKE_COLOR,
     borderRadius: 25,
