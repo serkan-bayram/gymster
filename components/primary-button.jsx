9f4061f36b961557a75f1a8b507216f6dc3318e2
Serkan <serkanbay02@hotmail.com>
2024-04-03 11:15:55 +0300

    remove expo dev client & fix forward ref bug on button

--- components/primary-button.jsx
+++ components/primary-button.jsx
@@ -1,13 +1,16 @@
 import { Pressable, Text } from "react-native";
 import { cn } from "../utils/cn";
+import { forwardRef } from "react";
 
-export function PrimaryButton({ text, className, ...props }) {
+export const PrimaryButton = forwardRef(function PrimaryButton(props, ref) {
+  const { text, className, ...otherProps } = props;
+
   return (
     <Pressable
-      {...props}
+      {...otherProps}
       className={cn(className, "w-full bg-secondary p-4 rounded-xl")}
     >
       <Text className="text-center text-white">{text}</Text>
     </Pressable>
   );
-}
+});
