f905a5226b13de7c2b9c5e7c9cce50b292a9297d
Serkan <serkanbay02@hotmail.com>
2024-05-06 16:11:34 +0300

    fix wentToGYM state is wrong when fetched bug

--- components/gym-days.jsx
+++ components/gym-days.jsx
@@ -1,13 +1,15 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { DaysHeading } from "./ui/gym-days/days-heading";
 import { DaysContainer } from "./ui/gym-days/days-container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getGYMDays, getServerTime } from "@/utils/db";
 import { useSession } from "@/utils/session-context";
 
 export function GYMDays() {
   const { session } = useSession();
+
+  const [wentToGYM, setWentToGYM] = useState(false);
 
   // This query gets the days that user went to gym
   const query = useQuery({
@@ -26,16 +28,14 @@ export function GYMDays() {
         // Did user went to gym today
         const wentToGYMToday = wentToGYMDays.includes(todaysDate);
 
+        setWentToGYM(wentToGYMToday);
+
         return { wentToGYMDays, wentToGYMToday };
       }
 
       return null;
     },
   });
-
-  const [wentToGYM, setWentToGYM] = useState(
-    query.data?.wentToGYMToday || false
-  );
 
   return (
     <View className="flex gap-y-2 mt-4 px-4">
