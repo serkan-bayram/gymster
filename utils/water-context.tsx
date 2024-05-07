5900927bee79899b9f689b50d4d63b50ad1b3cb3
Serkan <serkanbay02@hotmail.com>
2024-05-07 03:44:05 +0300

    more js to ts conversion

--- utils/water-context.tsx
+++ utils/water-context.tsx
@@ -1,14 +1,29 @@
 import { createContext, useContext, useState } from "react";
 
-const WaterContext = createContext();
+// TODO: Learn more about these types
+const WaterContext = createContext<{
+  updateValue: number;
+  setUpdateValue: React.Dispatch<React.SetStateAction<number>>;
+  goalValue: number;
+  setGoalValue: React.Dispatch<React.SetStateAction<number>>;
+}>({
+  updateValue: 0,
+  setUpdateValue: () => {},
+  goalValue: 0,
+  setGoalValue: () => {},
+});
 
 // Increase or decrase the progress by 200 ml
 export const DEFAULT_UPDATE_VALUE = 200;
 // Default goal is 2000 ml
 export const DEFAULT_GOAL = 2000;
 
+type WaterProviderType = {
+  children: React.ReactNode;
+};
+
 // TODO: Save goal and update values to local storage
-export const WaterProvider = ({ children }) => {
+export const WaterProvider = ({ children }: WaterProviderType) => {
   const [updateValue, setUpdateValue] = useState(DEFAULT_UPDATE_VALUE);
   const [goalValue, setGoalValue] = useState(DEFAULT_GOAL);
 
